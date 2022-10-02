6b6a542b789b9f860f1908309614d4031d77a6a3
Viktoriia Z <viktoria.dubinina@gmail.com>
2022-10-02 12:16:40 -0700

    add state function and basic html for characters

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
     new CleanWebpackPlugin(),
     new Dotenv(),
     new HtmlWebpackPlugin({
-      title: 'Shape Tracker',
+      title: 'RPG-with-functional-techniques',
       template: './src/index.html',
       inject: 'body'
     })
