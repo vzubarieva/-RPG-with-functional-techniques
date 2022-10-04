a6894d5803fbaf62b97dea7605dcbeb7d2886c9c
Viktoriia Z <viktoria.dubinina@gmail.com>
2022-10-03 20:45:23 -0700

    complete functionality for getHealth, still working on using dropdown list and its value functionality

--- src/index.js
+++ src/index.js
@@ -3,16 +3,22 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/styles.css";
 import {
   hitPlayer1,
-  hitPlayer2,
+  // hit2Player2,
+  getMoreHealthPlayer1,
+  getMoreHealthPlayer2,
   getHealthPlayer1,
   getHealthPlayer2,
   stateControl,
 } from "./js/character.js";
 
 $(document).ready(function () {
+  //$("#weapon1").val();
+  //"rock1" || "bow1" || "sword1"
+
   $("#character-1-hit").click(function () {
-    const state = stateControl(hitPlayer1);
-    $("#character-2-characterPoints").text(`${state.rock_player1}`);
+    const valPlayer1 = $("#weapons1").val();
+    const state = stateControl(valPlayer1); //hitPlayer2
+    $("#character-2-characterPoints").text(`${state.rock_player2}`);
   });
 
   $("#character-1-getHealth").click(function () {
@@ -20,13 +26,24 @@ $(document).ready(function () {
     $("#character-1-characterPoints").text(`${state.rock_player1}`);
   });
 
+  $("#character-1-getEvenMoreHealth").click(function () {
+    const state = stateControl(getMoreHealthPlayer1);
+    $("#character-1-characterPoints").text(`${state.rock_player1}`);
+  });
+
+  //$("#weapon2").val();
   $("#character-2-hit").click(function () {
-    const state = stateControl(hitPlayer2);
-    $("#character-1-characterPoints").text(`${state.rock_player2}`);
+    const state = stateControl(hitPlayer1);
+    $("#character-1-characterPoints").text(`${state.rock_player1}`);
   });
 
   $("#character-2-getHealth").click(function () {
     const state = stateControl(getHealthPlayer2);
+    $("#character-2-characterPoints").text(`${state.rock_player2}`);
+  });
+
+  $("#character-2-getEvenMoreHealth").click(function () {
+    const state = stateControl(getMoreHealthPlayer2);
     $("#character-2-characterPoints").text(`${state.rock_player2}`);
   });
 });
--- src/js/character.js
+++ src/js/character.js
@@ -32,21 +32,21 @@ export const storeListState = () => {
 };
 
 export const hitPlayer1 = changeCharacterState("rock_player1")(-2);
-export const hit2Player1 = changeCharacterState("bow_player1")(-5);
-export const hit3Player1 = changeCharacterState("sword_player1")(-7);
+export const hit2Player1 = changeCharacterState("rock_player1")(-5);
+export const hit3Player1 = changeCharacterState("rock_player1")(-7);
 
 export const getHealthPlayer1 = changeCharacterState("rock_player1")(3);
-export const getMoreHealthPlayer1 = changeCharacterState("eat_food_player1")(7);
+export const getMoreHealthPlayer1 = changeCharacterState("rock_player1")(7);
 export const getEvenMoreHealthPlayer1 = changeCharacterState(
   "super_health_player1"
 )(15);
 
 export const hitPlayer2 = changeCharacterState("rock_player2")(-2);
-export const hit2Player2 = changeCharacterState("bow_player2")(-5);
-export const hit3Player2 = changeCharacterState("sword_player2")(-7);
+export const hit2Player2 = changeCharacterState("rock_player2")(-5);
+export const hit3Player2 = changeCharacterState("rock_player2")(-7);
 
 export const getHealthPlayer2 = changeCharacterState("rock_player2")(3);
-export const getMoreHealthPlayer2 = changeCharacterState("eat_food_player2")(7);
+export const getMoreHealthPlayer2 = changeCharacterState("rock_player2")(7);
 export const getEvenMoreHealthPlayer2 = changeCharacterState(
   "super_health_player2"
 )(15);
